95fcdc24cda7aff69dd11e471c4ec7e20b8aadeb
nzbenji <benlittle53@gmail.com>
2020-08-16 13:16:37 +1200

    added SVG images for reporting & timesheeting

--- components/Services.js
+++ components/Services.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import TimeSVG from '../images/TimeSVG'
+
 export default function Services() {
     return (
-        <section class="pb-20 bg-gray-300 -mt-24">
+        <section class="pb-20  -mt-24">
             <div class="container mx-auto px-4">
                 <div class="flex flex-wrap">
                     <div class="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
@@ -15,7 +17,7 @@ export default function Services() {
                                 >
                                     <i class="fas fa-award"></i>
                                 </div>
-                                <h6 class="text-xl font-semibold">Awarded Agency</h6>
+                                <h6 class="text-xl font-semibold">Mobile Applications</h6>
                                 <p class="mt-2 mb-4 text-gray-600">
                                     Divide details about your product or agency work into parts.
                                     A paragraph describing a feature will be enough.
@@ -33,7 +35,7 @@ export default function Services() {
                                 >
                                     <i class="fas fa-retweet"></i>
                                 </div>
-                                <h6 class="text-xl font-semibold">Free Revisions</h6>
+                                    <h6 class="text-xl font-semibold">Business Automation</h6>
                                 <p class="mt-2 mb-4 text-gray-600">
                                     Keep you user engaged by providing meaningful information.
                                     Remember that by this time, the user is curious.
@@ -51,7 +53,7 @@ export default function Services() {
                                 >
                                     <i class="fas fa-fingerprint"></i>
                                 </div>
-                                <h6 class="text-xl font-semibold">Verified Company</h6>
+                                <h6 class="text-xl font-semibold">Web Applications</h6>
                                 <p class="mt-2 mb-4 text-gray-600">
                                     Write a few lines about each one. A paragraph describing a
                                     feature will be enough. Keep you user engaged!
@@ -67,62 +69,23 @@ export default function Services() {
                         >
                             <i class="fas fa-user-friends text-xl"></i>
                         </div>
-                        <h3 class="text-3xl mb-2 font-semibold leading-normal">
-                            Working with us is a pleasure
+                        <h3 class="text-3xl mb-2 font-semibold leading-normal text-center">
+                            Restore clarity with automated time tracking
               </h3>
                         <p
                             class="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-700"
                         >
-                            Don't let your uses guess by attaching tooltips and popoves to
-                            any element. Just make sure you enable them first via
-                            JavaScript.
+                            Are you struggling to keep track of your team's performance across different jobs? 
+                            Say "goodbye" to spreadsheet hell and guesswork. Let **** keep track of your business's 
+                            time tracking and focus on work that matters.
               </p>
-                        <p
-                            class="text-lg font-light leading-relaxed mt-0 mb-4 text-gray-700"
-                        >
-                            The kit comes with three pre-built pages to help you get started
-                            faster. You can change the text and images and you're good to
-                            go. Just make sure you enable them first via JavaScript.
-              </p>
-                        <a
-                            href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit#/presentation"
-                            class="font-bold text-gray-800 mt-8"
-                        >Check Tailwind Starter Kit!</a
-                        >
                     </div>
                     <div class="w-full md:w-4/12 px-4 mr-auto ml-auto">
-                        <div
-                            class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-pink-600"
-                        >
-                            <img
-                                alt="..."
-                                src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1051&amp;q=80"
-                                class="w-full align-middle rounded-t-lg"
-                            />
-                            <blockquote class="relative p-8 mb-4">
-                                <svg
-                                    preserveAspectRatio="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 583 95"
-                                    class="absolute left-0 w-full block"
-                                    //style="height: 95px; top: -94px;"
-                                    style={{ height: '95px', top: '-94px' }}
-                                >
-                                    <polygon
-                                        points="-30,95 583,95 583,65"
-                                        class="text-pink-600 fill-current"
-                                    ></polygon>
-                                </svg>
-                                <h4 class="text-xl font-bold text-white">
-                                    Top Notch Services
-                  </h4>
-                                <p class="text-md font-light mt-2 text-white">
-                                    The Arctic Ocean freezes every winter and much of the
-                                    sea-ice then thaws every summer, and that process will
-                                    continue whatever happens.
-                  </p>
-                            </blockquote>
-                        </div>
+                        {/* <div
+                            class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg"
+                        > */}
+                        <TimeSVG />
+                        {/* </div> */}
                     </div>
                 </div>
             </div>
